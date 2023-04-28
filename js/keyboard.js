e8a0606b099d6705eaef516efca626260afad683
Henadzi Rudchenko <grudchenko93@yandex.by>
2023-04-28 09:33:48 +0300

    feat: added caps

---
+++ js/keyboard.js
@@ -0,0 +1,957 @@
+export const keyboard = {
+  row1: {
+    Backquote: {
+      name: 'Backquote',
+      eng: {
+        normal: '`',
+        shift: '~',
+        caps: '`',
+        shiftCaps: '~',
+      },
+      rus: {
+        normal: 'ё',
+        shift: 'Ё',
+        caps: 'Ё',
+        shiftCaps: 'ё',
+      }
+    },
+    Digit1: {
+      name: 'Digit1',
+      eng: {
+        normal: '1',
+        shift: '!',
+        caps: '1',
+        shiftCaps: '!',
+      },
+      rus: {
+        normal: '1',
+        shift: '!',
+        caps: '1',
+        shiftCaps: '!',
+      }
+    },
+    Digit2: {
+      name: 'Digit2',
+      eng: {
+        normal: '2',
+        shift: '@',
+        caps: '2',
+        shiftCaps: '@',
+      },
+      rus: {
+        normal: '2',
+        shift: '""',
+        caps: '2',
+        shiftCaps: '""',
+      }
+    },
+    Digit3: {
+      name: 'Digit3',
+      eng: {
+        normal: '3',
+        shift: '#',
+        caps: '3',
+        shiftCaps: '#',
+      },
+      rus: {
+        normal: '3',
+        shift: '№',
+        caps: '3',
+        shiftCaps: '№',
+      }
+    },
+    Digit4: {
+      name: 'Digit4',
+      eng: {
+        normal: '4',
+        shift: '$',
+        caps: '4',
+        shiftCaps: '$',
+      },
+      rus: {
+        normal: '4',
+        shift: ';',
+        caps: '4',
+        shiftCaps: ';',
+      }
+    },
+    Digit5: {
+      name: 'Digit5',
+      eng: {
+        normal: '5',
+        shift: '%',
+        caps: '5',
+        shiftCaps: '%',
+      },
+      rus: {
+        normal: '5',
+        shift: '%',
+        caps: '5',
+        shiftCaps: '%',
+      }
+    },
+    Digit6: {
+      name: 'Digit6',
+      eng: {
+        normal: '6',
+        shift: '^',
+        caps: '6',
+        shiftCaps: '^',
+      },
+      rus: {
+        normal: '6',
+        shift: ':',
+        caps: '6',
+        shiftCaps: ':',
+      }
+    },
+    Digit7: {
+      name: 'Digit7',
+      eng: {
+        normal: '7',
+        shift: '&',
+        caps: '7',
+        shiftCaps: '&',
+      },
+      rus: {
+        normal: '7',
+        shift: '?',
+        caps: '7',
+        shiftCaps: '?',
+      }
+    },
+    Digit8: {
+      name: 'Digit8',
+      eng: {
+        normal: '8',
+        shift: '*',
+        caps: '8',
+        shiftCaps: '*',
+      },
+      rus: {
+        normal: '8',
+        shift: '*',
+        caps: '8',
+        shiftCaps: '*',
+      }
+    },
+    Digit9: {
+      name: 'Digit9',
+      eng: {
+        normal: '9',
+        shift: '(',
+        caps: '9',
+        shiftCaps: '(',
+      },
+      rus: {
+        normal: '9',
+        shift: '(',
+        caps: '9',
+        shiftCaps: '(',
+      }
+    },
+    Digit0: {
+      name: 'Digit0',
+      eng: {
+        normal: '0',
+        shift: ')',
+        caps: '0',
+        shiftCaps: ')',
+      },
+      rus: {
+        normal: '0',
+        shift: ')',
+        caps: '0',
+        shiftCaps: ')',
+      }
+    },
+    Minus: {
+      name: 'Minus',
+      eng: {
+        normal: '-',
+        shift: '_',
+        caps: '-',
+        shiftCaps: '_',
+      },
+      rus: {
+        normal: '-',
+        shift: '_',
+        caps: '-',
+        shiftCaps: '_',
+      }
+    },
+    Equal: {
+      name: 'Equal',
+      eng: {
+        normal: '=',
+        shift: '+',
+        caps: '=',
+        shiftCaps: '+',
+      },
+      rus: {
+        normal: '=',
+        shift: '+',
+        caps: '=',
+        shiftCaps: '+',
+      }
+    },
+    Backspace: {
+      name: 'Backspace',
+      eng: {
+        normal: 'Backspace',
+        shift: 'Backspace',
+        caps: 'Backspace',
+        shiftCaps: 'Backspace',
+      },
+      rus: {
+        normal: 'Backspace',
+        shift: 'Backspace',
+        caps: 'Backspace',
+        shiftCaps: 'Backspace',
+      }
+    }
+  },
+  row2: {
+    Tab: {
+      name: 'Tab',
+      eng: {
+        normal: 'Tab',
+        shift: 'Tab',
+        caps: 'Tab',
+        shiftCaps: 'Tab',
+      },
+      rus: {
+        normal: 'Tab',
+        shift: 'Tab',
+        caps: 'Tab',
+        shiftCaps: 'Tab',
+      }
+    },
+    KeyQ: {
+      name: 'KeyQ',
+      eng: {
+        normal: 'q',
+        shift: 'Q',
+        caps: 'Q',
+        shiftCaps: 'q',
+      },
+      rus: {
+        normal: 'й',
+        shift: 'Й',
+        caps: 'Й',
+        shiftCaps: 'й',
+      }
+    },
+    KeyW: {
+      name: 'KeyW',
+      eng: {
+        normal: 'w',
+        shift: 'W',
+        caps: 'W',
+        shiftCaps: 'w',
+      },
+      rus: {
+        normal: 'ц',
+        shift: 'Ц',
+        caps: 'Ц',
+        shiftCaps: 'ц',
+      }
+    },
+    KeyE: {
+      name: 'KeyE',
+      eng: {
+        normal: 'e',
+        shift: 'E',
+        caps: 'E',
+        shiftCaps: 'e',
+      },
+      rus: {
+        normal: 'у',
+        shift: 'У',
+        caps: 'У',
+        shiftCaps: 'у',
+      }
+    },
+    KeyR: {
+      name: 'KeyR',
+      eng: {
+        normal: 'r',
+        shift: 'R',
+        caps: 'R',
+        shiftCaps: 'r',
+      },
+      rus: {
+        normal: 'к',
+        shift: 'К',
+        caps: 'К',
+        shiftCaps: 'к',
+      }
+    },
+    KeyT: {
+      name: 'KeyT',
+      eng: {
+        normal: 't',
+        shift: 'T',
+        caps: 'T',
+        shiftCaps: 't',
+      },
+      rus: {
+        normal: 'е',
+        shift: 'Е',
+        caps: 'Е',
+        shiftCaps: 'е',
+      }
+    },
+    KeyY: {
+      name: 'KeyY',
+      eng: {
+        normal: 'y',
+        shift: 'Y',
+        caps: 'Y',
+        shiftCaps: 'y',
+      },
+      rus: {
+        normal: 'н',
+        shift: 'Н',
+        caps: 'Н',
+        shiftCaps: 'н',
+      }
+    },
+    KeyU: {
+      name: 'KeyU',
+      eng: {
+        normal: 'u',
+        shift: 'U',
+        caps: 'U',
+        shiftCaps: 'u',
+      },
+      rus: {
+        normal: 'г',
+        shift: 'Г',
+        caps: 'Г',
+        shiftCaps: 'г',
+      }
+    },
+    KeyI: {
+      name: 'KeyI',
+      eng: {
+        normal: 'i',
+        shift: 'I',
+        caps: 'I',
+        shiftCaps: 'i',
+      },
+      rus: {
+        normal: 'ш',
+        shift: 'Ш',
+        caps: 'Ш',
+        shiftCaps: 'ш',
+      }
+    },
+    KeyO: {
+      name: 'KeyO',
+      eng: {
+        normal: 'o',
+        shift: 'O',
+        caps: 'O',
+        shiftCaps: 'o',
+      },
+      rus: {
+        normal: '9',
+        shift: '(',
+        caps: '9',
+        shiftCaps: '(',
+      }
+    },
+    KeyP: {
+      name: 'KeyP',
+      eng: {
+        normal: 'p',
+        shift: 'P',
+        caps: 'P',
+        shiftCaps: 'p',
+      },
+      rus: {
+        normal: '0',
+        shift: ')',
+        caps: '0',
+        shiftCaps: ')',
+      }
+    },
+    BracketLeft: {
+      name: 'BracketLeft',
+      eng: {
+        normal: '[',
+        shift: '{',
+        caps: '{',
+        shiftCaps: '[',
+      },
+      rus: {
+        normal: '[',
+        shift: '{',
+        caps: '{',
+        shiftCaps: '[',
+      }
+    },
+    BracketRight: {
+      name: 'BracketRight',
+      eng: {
+        normal: ']',
+        shift: '}',
+        caps: '}',
+        shiftCaps: ']',
+      },
+      rus: {
+        normal: ']',
+        shift: '}',
+        caps: '}',
+        shiftCaps: ']',
+      }
+    },
+    Backslash: {
+      name: 'Backslash',
+      eng: {
+        normal: '\\',
+        shift: '|',
+        caps: '|',
+        shiftCaps: '\\',
+      },
+      rus: {
+        normal: '\\',
+        shift: '|',
+        caps: '|',
+        shiftCaps: '\\',
+      }
+    }
+  },
+  row3: {
+    CapsLock: {
+      name: 'CapsLock',
+      eng: {
+        normal: 'Capslock',
+        shift: 'Capslock',
+        caps: 'Capslock',
+        shiftCaps: 'Capslock',
+      },
+      rus: {
+        normal: 'Capslock',
+        shift: 'Capslock',
+        caps: 'Capslock',
+        shiftCaps: 'Capslock',
+      }
+    },
+    KeyA: {
+      name: 'KeyA',
+      eng: {
+        normal: 'a',
+        shift: 'A',
+        caps: 'A',
+        shiftCaps: 'a',
+      },
+      rus: {
+        normal: 'ф',
+        shift: 'Ф',
+        caps: 'Ф',
+        shiftCaps: 'ф',
+      }
+    },
+    KeyS: {
+      name: 'KeyS',
+      eng: {
+        normal: 's',
+        shift: 'S',
+        caps: 'S',
+        shiftCaps: 's',
+      },
+      rus: {
+        normal: 'ы',
+        shift: 'Ы',
+        caps: 'Ы',
+        shiftCaps: 'ы',
+      }
+    },
+    KeyD: {
+      name: 'KeyD',
+      eng: {
+        normal: 'd',
+        shift: 'D',
+        caps: 'D',
+        shiftCaps: 'd',
+      },
+      rus: {
+        normal: 'в',
+        shift: 'В',
+        caps: 'В',
+        shiftCaps: 'в',
+      }
+    },
+    KeyF: {
+      name: 'KeyF',
+      eng: {
+        normal: 'f',
+        shift: 'F',
+        caps: 'F',
+        shiftCaps: 'f',
+      },
+      rus: {
+        normal: 'а',
+        shift: 'А',
+        caps: 'А',
+        shiftCaps: 'а',
+      }
+    },
+    KeyG: {
+      name: 'KeyG',
+      eng: {
+        normal: 'g',
+        shift: 'G',
+        caps: 'G',
+        shiftCaps: 'g',
+      },
+      rus: {
+        normal: 'п',
+        shift: 'П',
+        caps: 'П',
+        shiftCaps: 'п',
+      }
+    },
+    KeyH: {
+      name: 'KeyH',
+      eng: {
+        normal: 'h',
+        shift: 'H',
+        caps: 'H',
+        shiftCaps: 'h',
+      },
+      rus: {
+        normal: 'р',
+        shift: 'Р',
+        caps: 'Р',
+        shiftCaps: 'р',
+      }
+    },
+    KeyJ: {
+      name: 'KeyJ',
+      eng: {
+        normal: 'j',
+        shift: 'J',
+        caps: 'J',
+        shiftCaps: 'j',
+      },
+      rus: {
+        normal: 'о',
+        shift: 'О',
+        caps: 'О',
+        shiftCaps: 'о',
+      }
+    },
+    KeyK: {
+      name: 'KeyK',
+      eng: {
+        normal: 'k',
+        shift: 'K',
+        caps: 'K',
+        shiftCaps: 'k',
+      },
+      rus: {
+        normal: 'л',
+        shift: 'Л',
+        caps: 'Л',
+        shiftCaps: 'л',
+      }
+    },
+    KeyL: {
+      name: 'KeyL',
+      eng: {
+        normal: 'l',
+        shift: 'L',
+        caps: 'L',
+        shiftCaps: 'l',
+      },
+      rus: {
+        normal: 'д',
+        shift: 'Д',
+        caps: 'Д',
+        shiftCaps: 'д',
+      }
+    },
+    Semicolon: {
+      name: 'Semicolon',
+      eng: {
+        normal: ';',
+        shift: ':',
+        caps: ':',
+        shiftCaps: ';',
+      },
+      rus: {
+        normal: 'ж',
+        shift: 'Ж',
+        caps: 'Ж',
+        shiftCaps: 'ж',
+      }
+    },
+    Quote: {
+      name: 'Quote',
+      eng: {
+        normal: '\'',
+        shift: '"',
+        caps: '"',
+        shiftCaps: '\'',
+      },
+      rus: {
+        normal: 'э',
+        shift: 'Э',
+        caps: 'Э',
+        shiftCaps: 'э',
+      }
+    },
+    Enter: {
+      name: 'Enter',
+      eng: {
+        normal: 'Enter',
+        shift: 'Enter',
+        caps: 'Enter',
+        shiftCaps: 'Enter',
+      },
+      rus: {
+        normal: 'Enter',
+        shift: 'Enter',
+        caps: 'Enter',
+        shiftCaps: 'Enter',
+      }
+    },
+  },
+  row4: {
+    ShiftLeft: {
+      name: 'ShiftLeft',
+      eng: {
+        normal: 'Shift',
+        shift: 'Shift',
+        caps: 'Shift',
+        shiftCaps: 'Shift',
+      },
+      rus: {
+        normal: 'Shift',
+        shift: 'Shift',
+        caps: 'Shift',
+        shiftCaps: 'Shift',
+      }
+    },
+    KeyZ: {
+      name: 'KeyZ',
+      eng: {
+        normal: 'z',
+        shift: 'Z',
+        caps: 'Z',
+        shiftCaps: 'z',
+      },
+      rus: {
+        normal: 'я',
+        shift: 'Я',
+        caps: 'Я',
+        shiftCaps: 'я',
+      }
+    },
+    KeyX: {
+      name: 'KeyX',
+      eng: {
+        normal: 'x',
+        shift: 'X',
+        caps: 'X',
+        shiftCaps: 'x',
+      },
+      rus: {
+        normal: 'ч',
+        shift: 'Ч',
+        caps: 'Ч',
+        shiftCaps: 'ч',
+      }
+    },
+    KeyC: {
+      name: 'KeyC',
+      eng: {
+        normal: 'c',
+        shift: 'C',
+        caps: 'C',
+        shiftCaps: 'c',
+      },
+      rus: {
+        normal: 'с',
+        shift: 'С',
+        caps: 'С',
+        shiftCaps: 'с',
+      }
+    },
+    KeyV: {
+      name: 'KeyV',
+      eng: {
+        normal: 'v',
+        shift: 'V',
+        caps: 'V',
+        shiftCaps: 'v',
+      },
+      rus: {
+        normal: 'м',
+        shift: 'М',
+        caps: 'М',
+        shiftCaps: 'м',
+      }
+    },
+    KeyB: {
+      name: 'KeyB',
+      eng: {
+        normal: 'b',
+        shift: 'B',
+        caps: 'B',
+        shiftCaps: 'b',
+      },
+      rus: {
+        normal: 'и',
+        shift: 'И',
+        caps: 'И',
+        shiftCaps: 'и',
+      }
+    },
+    KeyN: {
+      name: 'KeyN',
+      eng: {
+        normal: 'n',
+        shift: 'N',
+        caps: 'N',
+        shiftCaps: 'n',
+      },
+      rus: {
+        normal: 'т',
+        shift: 'Т',
+        caps: 'Т',
+        shiftCaps: 'т',
+      }
+    },
+    KeyM: {
+      name: 'KeyM',
+      eng: {
+        normal: 'm',
+        shift: 'M',
+        caps: 'M',
+        shiftCaps: 'm',
+      },
+      rus: {
+        normal: 'ь',
+        shift: 'Ь',
+        caps: 'Ь',
+        shiftCaps: 'ь',
+      }
+    },
+    Comma: {
+      name: 'Comma',
+      eng: {
+        normal: ',',
+        shift: '<',
+        caps: '<',
+        shiftCaps: ',',
+      },
+      rus: {
+        normal: 'б',
+        shift: 'Б',
+        caps: 'Б',
+        shiftCaps: 'б',
+      }
+    },
+    Period: {
+      name: 'Period',
+      eng: {
+        normal: '.',
+        shift: '>',
+        caps: '>',
+        shiftCaps: '.',
+      },
+      rus: {
+        normal: 'ю',
+        shift: 'Ю',
+        caps: 'Ю',
+        shiftCaps: 'ю',
+      }
+    },
+    Slash: {
+      name: 'Slash',
+      eng: {
+        normal: '/',
+        shift: '?',
+        caps: '?',
+        shiftCaps: '/',
+      },
+      rus: {
+        normal: '.',
+        shift: ',',
+        caps: ',',
+        shiftCaps: '.',
+      }
+    },
+    ArrowUp: {
+      name: 'ArrowUp',
+      eng: {
+        normal: '▲',
+        shift: '▲',
+        caps: '▲',
+        shiftCaps: '▲',
+      },
+      rus: {
+        normal: '▲',
+        shift: '▲',
+        caps: '▲',
+        shiftCaps: '▲',
+      }
+    },
+    ShiftRight: {
+      name: 'ShiftRight',
+      eng: {
+        normal: 'Shift',
+        shift: 'Shift',
+        caps: 'Shift',
+        shiftCaps: 'Shift',
+      },
+      rus: {
+        normal: 'Shift',
+        shift: 'Shift',
+        caps: 'Shift',
+        shiftCaps: 'Shift',
+      }
+    },
+  },
+  row5: {
+    ControlLeft: {
+      name: 'ControlLeft',
+      eng: {
+        normal: 'Ctrl',
+        shift: 'Ctrl',
+        caps: 'Ctrl',
+        shiftCaps: 'Ctrl',
+      },
+      rus: {
+        normal: 'Ctrl',
+        shift: 'Ctrl',
+        caps: 'Ctrl',
+        shiftCaps: 'Ctrl',
+      }
+    },
+    MetaLeft: {
+      name: 'MetaLeft',
+      eng: {
+        normal: 'Win',
+        shift: 'Win',
+        caps: 'Win',
+        shiftCaps: 'Win',
+      },
+      rus: {
+        normal: 'Win',
+        shift: 'Win',
+        caps: 'Win',
+        shiftCaps: 'Win',
+      }
+    },
+    AltLeft: {
+      name: 'AltLeft',
+      eng: {
+        normal: 'Alt',
+        shift: 'Alt',
+        caps: 'Alt',
+        shiftCaps: 'Alt',
+      },
+      rus: {
+        normal: 'Alt',
+        shift: 'Alt',
+        caps: 'Alt',
+        shiftCaps: 'Alt',
+      }
+    },
+    Space: {
+      name: 'Space',
+      eng: {
+        normal: ' ',
+        shift: ' ',
+        caps: ' ',
+        shiftCaps: ' ',
+      },
+      rus: {
+        normal: ' ',
+        shift: ' ',
+        caps: ' ',
+        shiftCaps: ' ',
+      }
+    },
+    AltRight: {
+      name: 'AltRight',
+      eng: {
+        normal: 'Alt',
+        shift: 'Alt',
+        caps: 'Alt',
+        shiftCaps: 'Alt',
+      },
+      rus: {
+        normal: 'Alt',
+        shift: 'Alt',
+        caps: 'Alt',
+        shiftCaps: 'Alt',
+      }
+    },
+    ArrowLeft: {
+      name: 'ArrowLeft',
+      eng: {
+        normal: '◄',
+        shift: '◄',
+        caps: '◄',
+        shiftCaps: '◄',
+      },
+      rus: {
+        normal: '◄',
+        shift: '◄',
+        caps: '◄',
+        shiftCaps: '◄',
+      }
+    },
+    ArrowDown: {
+      name: 'ArrowDown',
+      eng: {
+        normal: '▼',
+        shift: '▼',
+        caps: '▼',
+        shiftCaps: '▼',
+      },
+      rus: {
+        normal: '▼',
+        shift: '▼',
+        caps: '▼',
+        shiftCaps: '▼',
+      }
+    },
+    ArrowRight: {
+      name: 'ArrowRight',
+      eng: {
+        normal: '►',
+        shift: '►',
+        caps: '►',
+        shiftCaps: '►',
+      },
+      rus: {
+        normal: '►',
+        shift: '►',
+        caps: '►',
+        shiftCaps: '►',
+      }
+    },
+    ControlRight: {
+      name: 'ControlRight',
+      eng: {
+        normal: 'Ctrl',
+        shift: 'Ctrl',
+        caps: 'Ctrl',
+        shiftCaps: 'Ctrl',
+      },
+      rus: {
+        normal: 'Ctrl',
+        shift: 'Ctrl',
+        caps: 'Ctrl',
+        shiftCaps: 'Ctrl',
+      }
+    },
+  },
+}
---
+++ js/main.js
@@ -0,0 +1,212 @@
+'use strict'
+
+import { keyboard } from "./keyboard.js";
+
+const keyboardKeys = Object.keys(keyboard)
+const keyboardKeysRow1 = Object.keys(keyboard.row1)
+const keyboardKeysRow2 = Object.keys(keyboard.row2)
+const keyboardKeysRow3 = Object.keys(keyboard.row3)
+const keyboardKeysRow4 = Object.keys(keyboard.row4)
+const keyboardKeysRow5 = Object.keys(keyboard.row5)
+const body = document.body
+
+const container = document.createElement('div')
+container.classList.add('container')
+const virtualTablo = document.createElement('textarea')
+virtualTablo.classList.add('virtual-tablo')
+const virtualKeyboard = document.createElement('div')
+virtualKeyboard.classList.add('virtual-keyboard')
+
+body.prepend(container)
+container.prepend(virtualKeyboard)
+container.prepend(virtualTablo)
+
+for(let i = 0; i < keyboardKeys.length; i++) {
+  virtualKeyboard.innerHTML += `
+    <div class="row"></row>
+  `
+}
+
+const rows = document.querySelectorAll('.row')
+
+for(let i = 0; i < keyboardKeysRow1.length; i++) {
+  rows[0].innerHTML += `
+    <div class="key ${keyboard.row1[keyboardKeysRow1[i]].name}">
+      <div class="eng active">
+        <span class="normal">${keyboard.row1[keyboardKeysRow1[i]].eng.normal}</span>
+        <span class="shift hidden">${keyboard.row1[keyboardKeysRow1[i]].eng.shift}</span>
+        <span class="caps hidden">${keyboard.row1[keyboardKeysRow1[i]].eng.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row1[keyboardKeysRow1[i]].eng.shiftCaps}</span>
+      </div>
+      <div class="rus">
+        <span class="normal">${keyboard.row1[keyboardKeysRow1[i]].rus.normal}</span>
+        <span class="shift hidden">${keyboard.row1[keyboardKeysRow1[i]].rus.shift}</span>
+        <span class="caps hidden">${keyboard.row1[keyboardKeysRow1[i]].rus.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row1[keyboardKeysRow1[i]].rus.shiftCaps}</span>
+      </div>
+    </div>
+  `
+}
+
+for(let i = 0; i < keyboardKeysRow2.length; i++) {
+  rows[1].innerHTML += `
+    <div class="key ${keyboard.row2[keyboardKeysRow2[i]].name}">
+      <div class="eng active">
+        <span class="normal">${keyboard.row2[keyboardKeysRow2[i]].eng.normal}</span>
+        <span class="shift hidden">${keyboard.row2[keyboardKeysRow2[i]].eng.shift}</span>
+        <span class="caps hidden">${keyboard.row2[keyboardKeysRow2[i]].eng.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row2[keyboardKeysRow2[i]].eng.shiftCaps}</span>
+      </div>
+      <div class="rus">
+        <span class="normal">${keyboard.row2[keyboardKeysRow2[i]].rus.normal}</span>
+        <span class="shift hidden">${keyboard.row2[keyboardKeysRow2[i]].rus.shift}</span>
+        <span class="caps hidden">${keyboard.row2[keyboardKeysRow2[i]].rus.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row2[keyboardKeysRow2[i]].rus.shiftCaps}</span>
+      </div>
+    </div>
+  `
+}
+
+for(let i = 0; i < keyboardKeysRow3.length; i++) {
+  rows[2].innerHTML += `
+    <div class="key ${keyboard.row3[keyboardKeysRow3[i]].name}">
+      <div class="eng active">
+        <span class="normal">${keyboard.row3[keyboardKeysRow3[i]].eng.normal}</span>
+        <span class="shift hidden">${keyboard.row3[keyboardKeysRow3[i]].eng.shift}</span>
+        <span class="caps hidden">${keyboard.row3[keyboardKeysRow3[i]].eng.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row3[keyboardKeysRow3[i]].eng.shiftCaps}</span>
+      </div>
+      <div class="rus">
+        <span class="normal">${keyboard.row3[keyboardKeysRow3[i]].rus.normal}</span>
+        <span class="shift hidden">${keyboard.row3[keyboardKeysRow3[i]].rus.shift}</span>
+        <span class="caps hidden">${keyboard.row3[keyboardKeysRow3[i]].rus.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row3[keyboardKeysRow3[i]].rus.shiftCaps}</span>
+      </div>
+    </div>
+  `
+}
+
+for(let i = 0; i < keyboardKeysRow4.length; i++) {
+  rows[3].innerHTML += `
+    <div class="key ${keyboard.row4[keyboardKeysRow4[i]].name}">
+      <div class="eng active">
+        <span class="normal">${keyboard.row4[keyboardKeysRow4[i]].eng.normal}</span>
+        <span class="shift hidden">${keyboard.row4[keyboardKeysRow4[i]].eng.shift}</span>
+        <span class="caps hidden">${keyboard.row4[keyboardKeysRow4[i]].eng.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row4[keyboardKeysRow4[i]].eng.shiftCaps}</span>
+      </div>
+      <div class="rus">
+        <span class="normal">${keyboard.row4[keyboardKeysRow4[i]].rus.normal}</span>
+        <span class="shift hidden">${keyboard.row4[keyboardKeysRow4[i]].rus.shift}</span>
+        <span class="caps hidden">${keyboard.row4[keyboardKeysRow4[i]].rus.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row4[keyboardKeysRow4[i]].rus.shiftCaps}</span>
+      </div>
+    </div>
+  `
+}
+
+for(let i = 0; i < keyboardKeysRow5.length; i++) {
+  rows[4].innerHTML += `
+    <div class="key ${keyboard.row5[keyboardKeysRow5[i]].name}">
+      <div class="eng active">
+        <span class="normal">${keyboard.row5[keyboardKeysRow5[i]].eng.normal}</span>
+        <span class="shift hidden">${keyboard.row5[keyboardKeysRow5[i]].eng.shift}</span>
+        <span class="caps hidden">${keyboard.row5[keyboardKeysRow5[i]].eng.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row5[keyboardKeysRow5[i]].eng.shiftCaps}</span>
+      </div>
+      <div class="rus">
+        <span class="normal">${keyboard.row5[keyboardKeysRow5[i]].rus.normal}</span>
+        <span class="shift hidden">${keyboard.row5[keyboardKeysRow5[i]].rus.shift}</span>
+        <span class="caps hidden">${keyboard.row5[keyboardKeysRow5[i]].rus.caps}</span>
+        <span class="shiftCaps hidden">${keyboard.row5[keyboardKeysRow5[i]].rus.shiftCaps}</span>
+      </div>
+    </div>
+  `
+}
+
+/* Прокидываю классы ширины кнопки */
+
+document.querySelector('.Backspace').classList.add('flex-1')
+document.querySelector('.Tab').classList.add('flex-1')
+document.querySelector('.CapsLock').classList.add('flex-1')
+document.querySelector('.ShiftRight').classList.add('flex-1')
+document.querySelector('.ShiftLeft').classList.add('flex-2')
+document.querySelector('.Enter').classList.add('flex-1')
+document.querySelector('.Space').classList.add('flex-4')
+document.querySelector('.Backslash').classList.add('flex-1')
+document.querySelector('.ControlLeft').classList.add('flex-1')
+document.querySelector('.ControlRight').classList.add('flex-1')
+document.querySelector('.AltLeft').classList.add('flex-1')
+document.querySelector('.AltRight').classList.add('flex-1')
+
+window.addEventListener('keydown', function(event) {
+  console.log(event.code)
+  document.querySelector(`.${event.code}`).classList.add('press')
+})
+
+window.addEventListener('keyup', function(event) {
+  document.querySelector(`.${event.code}`).classList.remove('press')
+})
+
+/* Нажатие Shift */
+
+window.addEventListener('keydown', function(event) {
+  if(event.code === 'ShiftLeft' && event.getModifierState('CapsLock') === false) {
+    document.querySelectorAll('.normal').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.shift').forEach(el => {
+      el.classList.remove('hidden')
+    })
+  } else if(event.code === 'ShiftLeft' && event.getModifierState('CapsLock') === true) {
+    document.querySelectorAll('.normal').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.shift').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.caps').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.shiftCaps').forEach(el => {
+      el.classList.remove('hidden')
+    })
+  }
+})
+
+window.addEventListener('keyup', function(event) {
+  if(event.code === 'ShiftLeft' && event.getModifierState('CapsLock') === false) {
+    document.querySelectorAll('.normal').forEach(el => {
+      el.classList.remove('hidden')
+    })
+    document.querySelectorAll('.shift').forEach(el => {
+      el.classList.add('hidden')
+    })
+  } else if(event.code === 'ShiftLeft' && event.getModifierState('CapsLock') === true) {
+    document.querySelectorAll('.normal').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.shift').forEach(el => {
+      el.classList.add('hidden')
+    })
+    document.querySelectorAll('.caps').forEach(el => {
+      el.classList.remove('hidden')
+    })
+    document.querySelectorAll('.shiftCaps').forEach(el => {
+      el.classList.add('hidden')
+    })
+  }
+})
+
+/* Нажатие CapsLock */
+
+window.addEventListener('keydown', function(event) {
+  if(event.code === 'CapsLock') {
+    document.querySelectorAll('.normal').forEach(el => {
+      el.classList.contains('hidden') ? el.classList.remove('hidden') : el.classList.add('hidden')
+    })
+    document.querySelectorAll('.caps').forEach(el => {
+      el.classList.contains('hidden') ? el.classList.remove('hidden') : el.classList.add('hidden')
+    })
+  }
+})
